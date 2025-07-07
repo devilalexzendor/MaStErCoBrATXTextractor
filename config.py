b661f267fe284e4bdb577a83b9679e1a82d32cc5
devilalexzendor <ALOKKUMAR534M@GMAIL.COM>
2025-07-07 23:34:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "--1002768582112"))
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002768582112")) 
 # -----------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://savebot:RxxBfvkv1xnGsbPL@cluster0.9zpw7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002376025556"))
 # -----------------------------------------------
